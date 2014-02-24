97ebe88e9f3039c37055ea3674d671a5568b39a6
oieioi <atsuinatsu.samuifuyu@gmail.com>
2014-02-24 17:13:41 +0900

    ruby

---
+++ ruby/fizz_buzz.rb
@@ -0,0 +1,10 @@
+def get_fizz_buzz(num)
+  return nil        unless num.is_a? Numeric
+  return "FizzBuzz" if     num % 15 == 0
+  return "Fizz"     if     num % 3  == 0
+  return "Buzz"     if     num % 5  == 0
+  return num
+end
+
+ret = (1..100).map {|num| get_fizz_buzz num}
+p ret.join ','
