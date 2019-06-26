01257ec71fe858310da9a73cc1fca8e0042c819f
oieioi <atsuinatsu.samuifuyu@gmail.com>
2019-06-26 17:58:05 +0900

    C

---
+++ fizz_buzz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *args[]) {
+  for(int i = 1; i <= 100; i++) {
+    if (i % 3 == 0) printf("Fizz");
+    if (i % 5 == 0) printf("Buzz");
+    if (i % 3 != 0 && i % 5 != 0) printf("%d", i);
+    printf(",");
+  }
+  return 0;
+}
