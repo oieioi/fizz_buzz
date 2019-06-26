32bfa2e6fe10c73fa01c78be112a94d20361debf
oieioi <atsuinatsu.samuifuyu@gmail.com>
2019-06-26 18:15:58 +0900

    Fix error when `ros fizz_buzz.lisp`

--- fizz_buzz.lisp
+++ fizz_buzz.lisp
@@ -1,34 +1,44 @@
-; 繰り返し
-(dotimes (i 100)
-  ; 局所変数の定義
-  (let ((num (+ i 1)))
-    (if (= 0 (mod num 3))
-      (format t "Fizz"))
-    (if (= 0 (mod num 5))
-      (format t "Buzz"))
-    (if (and (not (= 0 (mod num 5)))
-             (not (= 0 (mod num 3))))
-      (format t "~a" num))
-    (format t " " )))
+; ros fizz_buzz.lisp
+(in-package :cl-user)
+(defpackage :cl-fizz-buzz (:use cl))
+(in-package :cl-fizz-buzz)
 
-; 関数でやる
-; counter 個のインクリメンタルなリストを作る
-(defun iota (counter &optional (start 0))
-  (if (= counter 0)
-    nil
-    (cons start (iota
-                  (- counter 1)
-                  (+ start 1)))))
+(defun main nil
+  ;;;;;;;;;;;;;;;;;;;;;;;;;
+  ; 繰り返しバージョン
+  ;;;;;;;;;;;;;;;;;;;;;;;;;
+  (dotimes (i 100)
+    ; 局所変数の定義
+    (let ((num (+ i 1)))
+      (if (= 0 (mod num 3))
+          (format t "Fizz"))
+      (if (= 0 (mod num 5))
+          (format t "Buzz"))
+      (if (and (not (= 0 (mod num 5)))
+               (not (= 0 (mod num 3))))
+          (format t "~a" num))
+      (format t " " )))
 
-; FizzBuzz を返す
-(defun fizz_buzz (num)
-  (cond ((and (= 0 (mod num 3)) (= 0 (mod num 5))) "FizzBuzz")
-        ((= 0 (mod num 3)) "Fizz")
-        ((= 0 (mod num 5)) "Buzz")
-        (t num)))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;
+  ; 関数でやるバージョン
+  ;;;;;;;;;;;;;;;;;;;;;;;;;
+  ; counter 個のインクリメンタルなリストを作る
+  (defun iota (counter &optional (start 0))
+    (if (= counter 0)
+        nil
+        (cons start (iota
+                      (- counter 1)
+                      (+ start 1)))))
 
-; 出力
-(format t
-        "~{~a ~}"
-        (mapcar #'fizz_buzz
-                (iota 100 1)))
+  ; FizzBuzz を返す
+  (defun fizz_buzz (num)
+    (cond ((and (= 0 (mod num 3)) (= 0 (mod num 5))) "FizzBuzz")
+          ((= 0 (mod num 3)) "Fizz")
+          ((= 0 (mod num 5)) "Buzz")
+          (t num)))
+
+  ; 出力
+  (format t
+          "~{~a ~}"
+          (mapcar #'fizz_buzz
+                  (iota 100 1))))
